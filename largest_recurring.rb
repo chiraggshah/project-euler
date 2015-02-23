cd2f388db66c044dcf7881c95801637c837bdb02
Chirag <chirag.shah@fab.com>
2015-02-24 03:30:39 +0530

    testing2

--- largest_recurring.rb
+++ largest_recurring.rb
@@ -1,4 +1,4 @@
-# project euler question 26.
+# project euler question 26
 # https://projecteuler.net/problem=26
 # Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
